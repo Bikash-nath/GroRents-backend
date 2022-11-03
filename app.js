b508048baea2d6c3b767d69ddb41ad596d244294
Bikash-nath <bikash625sf@gmail.com>
2022-11-03 20:53:40 +0530

    Add Load data

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev')); //to log req/res info
 }
 
 app.use(compression());
---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const House = require('./../../models/houseModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const houses = JSON.parse(fs.readFileSync(`${__dirname}/houses.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await House.create(houses);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await House.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
