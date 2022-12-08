f05007ab0eac730db71c41c13ce197d23fd19ef8
Bikash-nath <bikash625sf@gmail.com>
2022-12-08 11:54:34 +0530

    Deploy prod

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// const DB = process.env.DATABASE_LOCAL;
 
 mongoose
   .connect(DB, {
